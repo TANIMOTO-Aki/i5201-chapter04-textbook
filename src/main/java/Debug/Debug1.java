4e297f07bf9a64e12fff820a5b0496bad6c72502
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-25 11:52:50 +0900

    練習問題２ Debug1.javaを作成しました

---
+++ src/main/java/Debug/Debug1.java
@@ -0,0 +1,14 @@
+public class Debug1 {
+    public static void main(String[] args) {
+        int i;
+        int sum;
+
+        i = 1;
+        sum = 0;
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    } 
+}
