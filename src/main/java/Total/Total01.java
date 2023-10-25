6fa662fe26ccee17d92adfe258a2d2b9dc226257
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-25 09:30:09 +0900

    １から１０までの和を求めるプログラムTotal01.javaを作成しました。

---
+++ src/main/java/Total/Total01.java
@@ -0,0 +1,8 @@
+public class Total01 {
+    public static void main(String[] args) {
+        int sum;                            // 総和を求めるエリア（sum）
+
+        sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        System.out.println(sum);            // 総和の表示
+    }
+}
