e84ca1d36e1d4abe6a8396b730656dfe9d0e7298
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-26 09:48:45 +0900

    二度読み法のアルゴリズムを使ったプログラムInputTotal1.javaを作成しました

---
+++ src/main/java/InputTotal/InputTotal1.java
@@ -0,0 +1,27 @@
+/*
+ * 入力された値（整数）の合計を求めるプログラム（InputTotal.java）
+ * 入力の終了は[Ctrl]+[Z]キーを入力して[Enter]
+ */
+import java.io.*;
+
+public class InputTotal1 {
+    public static void main(String[] args) throws IOException {
+
+        /* 入力の準備 */
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("整数を連続して入力してください");
+        System.out.println("終了は[Ctrl]+[Z]を入力して[Enter]");
+
+        int sum = 0;                                // 総和の累計エリア(sum)
+
+        /* 入力の繰り返し */
+        String buf = br.readLine();                   // 数字列（整数）の入力
+        while (buf != null) {                       // 入力があれば繰り返し
+            sum += Integer.parseInt(buf);           // 入力値の加算
+            buf = br.readLine();                    // 数字列（整数）の入力
+        }
+        
+        System.out.println("合計は" + sum);
+    }
+}
