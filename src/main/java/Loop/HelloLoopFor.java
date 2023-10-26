6a66303d1c5a32baf576adbeacc768745655524b
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-26 11:04:44 +0900

    確認問題4.7 問題１ HelloLoopFor.javaを作成しました

---
+++ src/main/java/Loop/HelloLoopFor.java
@@ -0,0 +1,12 @@
+public class HelloLoopFor {
+    public static void main(String[] args) {
+        int i;                              // カウンタの宣言
+
+        for (i = 5; i > 0; i--) {           // ループ処理
+            System.out.println("Hello!"); // Hello!を表示
+        }
+    }
+}
+/*
+ * Hello!は5回表示される
+ */
