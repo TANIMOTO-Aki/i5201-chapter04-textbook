2d67b025d1f5134e0d557418d95c85485a6aacb2
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-31 14:28:50 +0900

    確認問題②　NestLoop.javaを作成しました

---
+++ src/main/java/Loop/NestLoop.java
@@ -0,0 +1,12 @@
+public class NestLoop {
+    public static void main(String[] args) {
+        
+    for (int i = 10; i > 0; i--) {
+            System.out.print("i=" + i + "\t");  // iの値とタブの表示
+            for (int j = i; j > 0; j--) {
+                System.out.print("〇");        // 〇の表示（繰り返し）
+            }
+            System.out.println();               // 改行
+        }
+    }
+}
